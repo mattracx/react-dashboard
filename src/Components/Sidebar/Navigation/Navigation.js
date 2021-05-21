c7a913e8f63f7ceb6ca07e74a1fd0bafaf306995
Matteo Raciti <1215814+mattracx@users.noreply.github.com>
2021-05-21 20:43:23 +0000

    Navigation Menu Initial

--- src/Components/Sidebar/Navigation/Navigation.js
+++ src/Components/Sidebar/Navigation/Navigation.js
@@ -1,16 +1,25 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { faChartPie, faLaptopCode, faWallet, faCommentDots } from "@fortawesome/free-solid-svg-icons";
 import './Navigation.css';
 
-class Navigation extends Component {
-    render() {
-        return <ul>
-            <li><FontAwesomeIcon icon={faHome}/> Overview</li>
-            <li><FontAwesomeIcon icon={faHome}/> Test2</li>
-            <li><FontAwesomeIcon icon={faHome}/> Test3</li>
-            <li><FontAwesomeIcon icon={faHome}/> Test4</li>
+export default function Navigation() {
+    const values = [
+        { id: 1, text: "Dashboard", icon: faChartPie },
+        { id: 2, text: "Overview", icon: faLaptopCode },
+        { id: 3, text: "Finance", icon: faWallet },
+        { id: 4, text: "Chat", icon: faCommentDots }
+      ];
+    const [activeId, setActiveId] = useState(1); // 1 == default page id
+
+    return (
+        <ul>
+            {values.map((val) => (
+                <li key={val.id} onClick={() => setActiveId(val.id)} className={activeId === val.id ? "active" : ""}>
+                   <FontAwesomeIcon icon={val.icon}/> {val.text} 
+                   {/* {activeId === val.id ? "Active" : "Inactive"} */}
+                </li>
+            ))}
         </ul>
-    }
-};
-export default Navigation;
+    )
+}
